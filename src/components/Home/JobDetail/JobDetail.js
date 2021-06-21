e8f893c10911a27e6793eb8a2f9025a77fdff4d8
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-06-21 21:44:58 +0600

    updated

--- src/components/Home/JobDetail/JobDetail.js
+++ src/components/Home/JobDetail/JobDetail.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './JobDetail.css';
 
 const JobDetail = ({job}) => {
+   
+   const handleSubmit = ()=>{
+       alert("Successfully applied");
+   }
     return (
         <div className="col-md-4 text-center mb-3">
-           <div class="card" styles="width: 18rem;">
-            <h4>{job.jobTitle}</h4>
+           <div className="job-card" class="card" styles="width: 18rem;">
+            <h4 className="p-2">{job.jobTitle}</h4>
             <div class="card-body">
                 <h5 class="card-title">{job.jobPosition}</h5>
                 <p class="card-text">{job.companyName}</p>
                 <p class="card-text">{job.location}</p>
                 <p class="card-text">{job.jobType}</p>
-                <a href="#htmlCard" class="btn btn-primary">Apply</a>
+                <a onClick={handleSubmit} href="#htmlCard" class="btn btn-primary">Apply</a>
             </div>
         </div>
         </div>
